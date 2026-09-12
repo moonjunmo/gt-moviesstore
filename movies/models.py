27dc76b657d5c492ce4a29a6acdef8366de4c9f0
Junmo Moon <moonjunmo@gatech.edu>
2026-09-12 17:31:04 -0400

    Initial commit

---
+++ movies/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='movie_images/')
+    def __str__(self):
+        return str(self.id) + ' - ' + self.name
+
+class Review(models.Model):
+    id = models.AutoField(primary_key=True)
+    comment = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.id) + ' - ' + self.movie.name
+
+class Report(models.Model):
+    id = models.AutoField(primary_key=True)
+    reason = models.CharField(max_length=255)
+    # If the user deletes their account, let's keep the report but null the user
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.id) + ' - ' + self.review.comment
